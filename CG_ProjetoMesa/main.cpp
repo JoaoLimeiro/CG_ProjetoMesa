b8d6ab386465701d4db9ee5acddd4da5fc9e4500
JoaoLimeiro <limeirowork@gmail.com>
2020-03-24 09:56:55 +0000

    Initial Commit

---
+++ CG_ProjetoMesa/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CG_ProjetoMesa
+//
+//  Created by João Limeiro on 24/03/2020.
+//  Copyright © 2020 João Limeiro. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
